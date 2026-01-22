3738020dc72dd1b4ab536768824136da77bb1769
Florian Kerbrat <Florian16534@gmail.com>
2026-01-22 20:17:20 +0100

    ajout de la fonction N chocs en M seconds

--- IMU_Reading/IMU_Reading.ino
+++ IMU_Reading/IMU_Reading.ino
@@ -1,13 +1,23 @@
+
+/*
+
+GROUPE 4 : KERBRAT Florian, VIDOR Julian
+
+*/
 #include <Arduino_LSM9DS1.h>
 #include <Wire.h>
 #include "DHT20.h"
 
 const unsigned long SAMPLE_PERIOD_MS = 100;
 const unsigned long DHT_PERIOD = 2000;
+
+//Seuil de déclenchement de l'alarme
 const float ACC_THRESHOLD = 1.2;
 const float TEMP_THRESHOLD = 28.0;
-const float M_last_seconds = 10;
+
+// utilisé pour la fonction check_speed qui ne prend pas en compte un valeur de temps M impartie pour réaliser les n chocs.
 const int n_shock_max = 10;
+// Cependant d'autre variables fonctions plus bas sont implémentés pour réaliser la verification des N chocs en M secondes
 
 // Pour faire la pause apres avoir appuyé sur le bouton
 const int time_break = 5000;
@@ -19,20 +29,42 @@ const int PIN_BUZZER = 12;
 const int PIN_BUTTON = A7;
 
 bool recording = false;
+bool alarm = false;
 
-
+// Variables de temps, utiliser pour faire un echantillons en respectant la période donnée (ex : now - lastSampleTime_acc < periode donnée)
 unsigned long lastSampleTime_acc = 0;
 unsigned long lastDHT = 0;
+
+//compte le nombre de chocs
 int shock_count = 0;
+
+// variable utilisé pour faire bipper l'alarme sur une periode donnée
 unsigned long lasTimeAlarmOn = 0;
 
+//derniere donnée de température et humidité enregistré
 float lastTemperature = 0;
 float lastHumidity = 0;
 
 String commandBuffer = "";
-bool alarm = false;
 
 DHT20 dht;
+
+///// Donnees pour la liste FIFO/////
+
+
+//Nombre de chocs (n=5) maximum en M secondes
+const int n_chocs = 5;
+const int m_secs = 10000;
+
+float liste_chocs[n_chocs];
+int head = 0;
+int tail = 0;
+
+
+
+///////////////////////////////////
+////////////Fonctions//////////////
+///////////////////////////////////
 
 void setup() {
   Serial.begin(115200);
@@ -99,23 +131,6 @@ void buttonCommand(unsigned long now) {
   }
 }
 
-/*
-void sendIMUSample(unsigned long now) {
-  float ax, ay, az;
-  if (!IMU.accelerationAvailable()) {
-    return;
-  }
-  IMU.readAcceleration(ax, ay, az);
-  Serial.print("t=");
-  Serial.print(now);
-  Serial.print(",ax=");
-  Serial.print(ax, 4);
-  Serial.print(",ay=");
-  Serial.print(ay, 4);
-  Serial.print(",az=");
-  Serial.println(az, 4);
-}
-*/
 
 void sendData(unsigned long now) {
 
@@ -141,17 +156,9 @@ void check_over_speed() {
   if (!IMU.accelerationAvailable()) {
     return;
   }
-
-
-
-
   IMU.readAcceleration(ax, ay, az);
-  //Serial.println("voici la valeur de l'acc : ");
-  //Serial.println(sqrt(ax * ax + ay * ay + az * az));
   if (sqrt(ax * ax + ay * ay + az * az) > ACC_THRESHOLD) {
     shock_count++;
-    //Serial.println("shock count :");
-    //Serial.print(shock_count);
   }
 }
 
@@ -171,14 +178,49 @@ void setAlarmOn(unsigned long now) {
     tone(PIN_BUZZER, 1000, 50);
     lasTimeAlarmOn = now;
   }
-
-  //if ((now / 500) % 2) tone(PIN_BUZZER, 1000, 50);
 }
 
 void setAlarmOff() {
   digitalWrite(PINLED, LOW);
   //le buzzer s'arrete tout seul
 }
+
+
+//////////////////////////////////////////////
+///////////Liste FIFO Fonctions///////////////
+
+void push(unsigned long now) {
+
+  float ax, ay, az;
+  if (!IMU.accelerationAvailable()) {
+    return;
+  }
+  IMU.readAcceleration(ax, ay, az);
+  if (sqrt(ax * ax + ay * ay + az * az) > ACC_THRESHOLD) {
+
+    liste_chocs[head] = now;
+    head = (head + 1) % n_chocs;
+ 
+    if (shock_count < n_chocs) {
+      shock_count ++;
+    } else {
+      tail = (tail + 1) % n_chocs;
+    }
+  }
+}
+
+float get_tail(){
+  float val_time = liste_chocs[tail];
+  return val_time;
+}
+
+void delete_queue() {
+  if (shock_count == 0) return;
+  tail = (tail + 1) % n_chocs;
+  shock_count --;
+}
+
+
 
 
 
@@ -194,6 +236,8 @@ void loop() {
     return;
   }
 
+/*
+  //Implémentation des chocs sans la prise en compte du temps imparti de M secondes
   //Check Over Speed
   if (now - lastSampleTime_acc >= SAMPLE_PERIOD_MS) {
     lastSampleTime_acc = now;
@@ -202,6 +246,17 @@ void loop() {
   if (shock_count >= n_shock_max) {
     alarm = true;
   }
+*/
+
+  //Check N chocs en M secondes
+  if (now - lastSampleTime_acc >= SAMPLE_PERIOD_MS ){
+    if(now - get_tail() > m_secs){
+      delete_queue();
+    }
+    lastSampleTime_acc = now;
+    push(now);
+  }
+
 
   //Check temperature
   if (now - lastDHT > DHT_PERIOD) {
@@ -210,11 +265,17 @@ void loop() {
     sendData(now);
   }
 
+  // Test dépassement de seuil de chocs ou de température
+  if (now - get_tail() < m_secs && shock_count >= n_chocs){
+    alarm = true;
+  }
   if (lastTemperature > TEMP_THRESHOLD) {
     alarm = true;
   }
 
-  if (lastTemperature < TEMP_THRESHOLD && shock_count < n_shock_max) {
+
+  //Sinon on remet l'alarm à False
+  if (lastTemperature < TEMP_THRESHOLD && shock_count < n_chocs) {
     alarm = false;
   }
 
